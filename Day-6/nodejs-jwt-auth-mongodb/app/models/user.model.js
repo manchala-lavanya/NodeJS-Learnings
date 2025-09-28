c170b8266a509a70a6aa8336300b886ad4a6253a
Manchala Lavanya <manchala.lavanya@cognizant.com>
2025-09-28 14:52:39 +0530

    Create user.model.js

---
+++ Day-6/nodejs-jwt-auth-mongodb/app/models/user.model.js
@@ -0,0 +1,16 @@
+// models/user.model.js
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role"
+    }
+  ]
+});
+
+module.exports = mongoose.model("User", UserSchema);
