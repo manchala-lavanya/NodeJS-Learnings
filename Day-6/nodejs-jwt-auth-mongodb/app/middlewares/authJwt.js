4811e0d5b3e1f174041fdb1922eee68f0ccd1382
Manchala Lavanya <manchala.lavanya@cognizant.com>
2025-09-28 14:49:18 +0530

    Create authJwt.js

---
+++ Day-6/nodejs-jwt-auth-mongodb/app/middlewares/authJwt.js
@@ -0,0 +1,71 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+verifyToken = (req, res, next) => {
+  const token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId).exec();
+    if (!user) {
+      return res.status(404).send({ message: "User not found." });
+    }
+
+    const roles = await Role.find({ _id: { $in: user.roles } }).exec();
+
+    for (let role of roles) {
+      if (role.name.toLowerCase() === "admin") {
+        return next();
+      }
+    }
+
+    return res.status(403).send({ message: "Require Admin Role!" });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+isModerator = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId).exec();
+    if (!user) {
+      return res.status(404).send({ message: "User not found." });
+    }
+
+    const roles = await Role.find({ _id: { $in: user.roles } }).exec();
+
+    for (let role of roles) {
+      if (role.name.toLowerCase() === "moderator") {
+        return next();
+      }
+    }
+
+    return res.status(403).send({ message: "Require Moderator Role!" });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const authJwt = {
+  verifyToken,
+  isAdmin,
+  isModerator
+};
+
+module.exports = authJwt;
